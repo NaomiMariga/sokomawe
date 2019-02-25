e2ed3bb9a4b304c71d2a158ac4e8527df8e7e8e1
ProfNaomi <nkoreste@gmail.com>
2019-02-25 20:04:11 +0300

    modify heroku environment

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, json # importing the flask module and class Flask is mandatory
 from user import Customer
+import os
 app = Flask(__name__) # Flask constructor takes the 
 
 customer = Customer() #instantiating the class
@@ -97,5 +98,6 @@ def logout():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
 

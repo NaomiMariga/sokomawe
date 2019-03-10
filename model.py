40dc0a1e0a653822bc9f3b2e86218df36d3b9fd5
Billcountry <me@billcountry.tech>
2019-03-10 10:49:55 +0300

    Removed JS like endings (Reinventing the wheel. Again. : http://whatthecommit.com)

--- model.py
+++ model.py
@@ -4,38 +4,36 @@ meta = MetaData()
 
 users = Table(
    'users', meta, 
-   Column('userid', Integer, primary_key = True), 
+   Column('userid', Integer, primary_key=True),
    Column('firstname', String), 
    Column('surname', String),
    Column('idnumber', String), 
    Column('phonenumber', String), 
    Column('email', String), 
    Column('username', String), 
-   Column('password', String), 
+   Column('password', String)
 )
 
 items = Table(
    'items', meta, 
    Column('itemid', Integer, primary_key = True), 
-   Column('userid', Integer,ForeignKey('users.userid', ondelete ='CASCADE')), 
+   Column('userid', Integer,ForeignKey('users.userid', ondelete='CASCADE')),
    Column('itemdescription', String),
    Column('price', String), 
    Column('shippingfee', String), 
-   Column('location', String), 
+   Column('location', String)
 )
 
 images = Table(
-'images', meta,
-Column('imageid',Integer, primary_key=True),
-Column('itemid', Integer, ForeignKey('users.userid', ondelete ='CASCADE')),
-Column('itemimagelink', String), 
-
+   'images', meta,
+   Column('imageid',Integer, primary_key=True),
+   Column('itemid', Integer, ForeignKey('users.userid', ondelete='CASCADE')),
+   Column('itemimagelink', String)
 )
 sessions = Table(
    'sessions', meta, 
-   Column('sessionid', Integer, primary_key = True), 
-   Column('userid', Integer, ForeignKey('users.userid', ondelete ='CASCADE')), 
-   Column('sessiontoken', String), 
-
+   Column('sessionid', Integer, primary_key=True),
+   Column('userid', Integer, ForeignKey('users.userid', ondelete='CASCADE')),
+   Column('sessiontoken', String)
 )
 meta.create_all(engine)

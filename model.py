52d984258804886ef541c091b5f8000185bab695
Billcountry <me@billcountry.tech>
2019-03-10 12:33:15 +0300

    I don't think that was necessary (after of this commit remember do a git reset hard : http://whatthecommit.com)

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
-engine = create_engine('postgres://vdjvyibgfeyiio:2d3c12435e93fe44ec5b7546dc7e494d5a0f7bac28b60bb325333ca1cd3a4e2c@ec2-23-21-165-188.compute-1.amazonaws.com:5432/ds3fhh163hsb1')
+engine = create_engine('postgres://vdjvyibgfeyiio:2d3c12435e93fe44ec5b7546dc7e494d5a0f7bac28b60bb325333ca1cd3a4e2c@ec2-23-21-165-188.compute-1.amazonaws.com:5432/ds3fhh163hsb1?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory')
 meta = MetaData()
 
 users = Table(
